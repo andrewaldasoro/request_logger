59a5241ba733102bb5ce93d82e2042f581190fa1
Kev Aldasoro <andrewaldasoro@gmail.com>
2021-08-16 12:45:33 -0400

    🥱 Try > Upload files with no content-type

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const yargs = require('yargs')
 
 const express = require('express')
+const multer = require('multer')
 const app = express()
 const upload = multer()
 
@@ -13,14 +14,28 @@ const argv = yargs
   })
   .argv
 
+app.use((req, res, next) => {
+  req.headers['content-type'] = 'multipart/form-data; boundary=----1234'
+  next()
+})
+
+app.put('/upload', upload.any(), (req, res) => {
+  // req.headers['content-type'] = 'multipart/form-data'
+  // console.log(req.get('Content-Type'))
+  // console.log(req.header('content-type'))
+  // if (!req.is()) req.type = 'multipart/form-data'
+  console.log(req.is())
+  if (req.file) console.log(req.file)
+  if (req.files) console.log(req.files)
+
+  res.send('')
+})
+
 app.all('/', (req, res) => {
   console.log(req);
   res.status(200).send('')
 })
 
-app.up('/upload', upload.any(), (req, res) => {
-  res.status(200).send('')
-})
 
 app.listen(argv.port, "0.0.0.0", () => {
   console.log(`Logger listening at http://localhost:${argv.port}`)
